da220eaa5e83cf4d2bfd22c36f1382047b233ea0
dvalverde <32370553+dvalverde@users.noreply.github.com>
2021-06-09 14:03:30 -0600

    Update

---
+++ Controllers/RespaldoController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExpDigital.ServerManagement;
+
+
+namespace ExpDigital.Controllers
+{
+    public class RespaldoController : Controller
+    {
+        Models.sp_ServerBackUpsQuerry_Result actual;
+        // GET: Respaldo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MenuRespaldo()
+        {
+            try
+            {
+                using (Models.ExpedienteDigitalEntities db = new Models.ExpedienteDigitalEntities())
+                {
+                    var listaRespaldos = db.sp_ServerBackUpsQuerry().ToList();
+                    return View(listaRespaldos);
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return View();
+            }
+        }
+
+        public ActionResult Detalles(int id)
+        {
+            try
+            {
+                using (Models.ExpedienteDigitalEntities db = new Models.ExpedienteDigitalEntities())
+                {
+
+                    var listaRespaldos = db.sp_ServerBackUpsQuerry().ToList();
+                    foreach (var item in listaRespaldos)
+                    {
+                        if (item.backup_set_id == id)
+                        {
+                            actual = item;
+                            return View(item);
+                        }
+                    }
+                    return MenuRespaldo();
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return MenuRespaldo();
+            }
+        }
+        [HttpPost]
+        public ActionResult Detalles(string path)
+        {
+            try
+            {
+                string[] partes = path.Split('\\');
+                ManejoBackUp mbu = new ManejoBackUp();
+                mbu.RestoreDB(partes[partes.Length - 1]);
+                ViewBag.restauracionExitosa = "Restauracion exitosa";
+                return Detalles(actual.backup_set_id);
+            }
+            catch (Exception e)
+            {
+                ViewBag.errorRestauracion = "Error : No se pudo usar el respaldo" + e;
+                return View();
+            }
+        }
+        public ActionResult CreateRespaldo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateRespaldo(string filename)
+        {
+            try
+            {
+                ManejoBackUp mbu = new ManejoBackUp();
+                mbu.CreateBackUp(filename);
+                ViewBag.nuevoRespaldo = "Backup creado" + filename;
+                return MenuRespaldo();
+            }
+            catch (Exception e)
+            {
+                ViewBag.errorRespaldo = "Error : No se pudo crear el respaldo " + e;
+                return View();
+            }
+        }
+    }
+}
---
+++ ServerManagement/ManejoBackUp.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.Data.SqlClient;
+
+namespace ExpDigital.ServerManagement
+{
+    public class ManejoBackUp
+    {
+        string con = "data source=DESKTOP-HC81J9H\\SQLEXPRESS;initial catalog=ExpedienteDigital;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"; //System.Configuration.ConfigurationManager.ConnectionStrings["ExpedienteDigitalEntities"].ConnectionString;
+        public void CreateBackUp(string fileName)
+        {
+            // Connect to the local, default instance of SQL Server.   
+            Server srv = new Server(new ServerConnection( new SqlConnection(con)));
+            // Reference the AdventureWorks2012 database.   
+            Database db = default(Database);
+            db = srv.Databases["ExpedienteDigital"];
+
+            // Store the current recovery model in a variable.   
+            int recoverymod;
+            recoverymod = (int)db.DatabaseOptions.RecoveryModel;
+
+            // Define a Backup object variable.   
+            Backup bk = new Backup();
+
+            // Specify the type of backup, the description, the name, and the database to be backed up.   
+            bk.Action = BackupActionType.Database;
+            bk.BackupSetDescription = "Full backup of ExpedienteDigital";
+            bk.BackupSetName = "ExpedienteDigital Backup";
+            bk.Database = "ExpedienteDigital";
+
+            // Declare a BackupDeviceItem by supplying the backup device file name in the constructor, and the type of device is a file.   
+            BackupDeviceItem bdi = default(BackupDeviceItem);
+            bdi = new BackupDeviceItem(fileName, DeviceType.File);
+
+            // Add the device to the Backup object.   
+            bk.Devices.Add(bdi);
+            // Set the Incremental property to False to specify that this is a full database backup.   
+            bk.Incremental = false;
+
+            // Set the expiration date.   
+            System.DateTime backupdate = new System.DateTime();
+            backupdate = DateTime.Today;
+            bk.ExpirationDate = backupdate;
+
+            // Specify that the log must be truncated after the backup is complete.   
+            bk.LogTruncation = BackupTruncateLogType.Truncate;
+
+            // Run SqlBackup to perform the full database backup on the instance of SQL Server.   
+            bk.SqlBackup(srv);
+
+            // Inform the user that the backup has been completed.   
+            System.Console.WriteLine("Full Backup complete.");
+
+            // Remove the backup device from the Backup object.   
+            bk.Devices.Remove(bdi);
+        }
+        public void RestoreDB(string fileName)
+        {
+            // Connect to the local, default instance of SQL Server.   
+            Server srv = new Server(new ServerConnection(new SqlConnection(con)));
+            // Reference the AdventureWorks2012 database.   
+            Database db = default(Database);
+            db = srv.Databases["ExpedienteDigital"];
+
+            // Store the current recovery model in a variable.   
+            int recoverymod;
+            recoverymod = (int)db.DatabaseOptions.RecoveryModel;
+
+            // Define a Restore object variable.  
+            Restore rs = new Restore();
+
+            // Set the NoRecovery property to true, so the transactions are not recovered.   
+            rs.NoRecovery = true;
+
+            // Declare a BackupDeviceItem by supplying the backup device file name in the constructor, and the type of device is a file.
+            BackupDeviceItem bdi = default(BackupDeviceItem);
+            bdi = new BackupDeviceItem(fileName, DeviceType.File);
+
+            // Add the device that contains the full database backup to the Restore object.   
+            rs.Devices.Add(bdi);
+
+            // Specify the database name.   
+            rs.Database = "ExpedienteDigital";
+
+            rs.ReplaceDatabase = true;
+
+            // Restore the full database backup with no recovery.   
+            rs.SqlRestore(srv);
+
+            // Inform the user that the Full Database Restore is complete.   
+            Console.WriteLine("Full Database Restore complete.");
+
+            // reacquire a reference to the database  
+            db = srv.Databases["ExpedienteDigital"];
+
+            // Remove the device from the Restore object.  
+            rs.Devices.Remove(bdi);
+
+            // Set the NoRecovery property to False.   
+            rs.NoRecovery = false;
+        }
+    }
+}
